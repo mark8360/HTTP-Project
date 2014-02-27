d1ed61cbc914e13af1b3f7ea8fb38012fcca5d0d
qitsuk <kasper.andreasen@gmail.com>
2014-02-27 10:42:29 +0100

    Beginnings

---
+++ SimpleHttpServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP_Server
+{
+    class SimpleHttpServer
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener serverSocket = new TcpListener(ip, 6789);
+
+            serverSocket.Start();
+
+            while (true)
+            {
+                Socket connectionSocket = serverSocket.AcceptSocket();
+                Console.WriteLine("Server now active.");
+            }
+        }
+    }
+}
